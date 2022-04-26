5ecd17256188d3b1f9df5dd2f9009c0ad07a5a9b
Torbjorn Jacobsen <torbjoern.jacobsen@hotmail.com>
2022-04-26 11:13:09 +0200

    comitting what I have when I got sick

--- BasicWebAPI/Controllers/WeatherForecastController.cs
+++ BasicWebAPI/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 
 public class WeatherforecastController : ControllerBase
 {
@@ -9,8 +10,80 @@ public class WeatherforecastController : ControllerBase
     {
         return new WeatherforecastDto
         {
-            DateTime = "01.01.2022"
+            Id = 1,
+            City = "Stavanger",
+            Date = DateTime.Now.Date,
+            Temperature = 14.3F,
+            Windspeed = 2.5F,
+            WindDirection = 293.8F,
+            WindspeedGust = 5.6F,
+            Pressure = 1023.7F,
+            Humidity = 42.4F,
+            ProbOfRain = 0F,
+            AmountRain = 0F,
+            CloudAreaFraction = 13.5F,
+            FogAreaFraction = 0F,
+            ProbOfThunder = 0F
         };
+    }
+
+    [HttpGet("{between}")]
+    public ActionResult<List<WeatherforecastDto>> Between(DateTime from, DateTime to)
+    {
+        string getStartDate = from.ToString().Substring(0, 5);
+        string getEndDate = to.ToString().Substring(0, 5);
+        double numberOfDays = 0;
+
+        if (getStartDate.Substring(3, 2) == getEndDate.Substring(3, 2))
+        {
+            double x = Convert.ToDouble(getStartDate.Substring(0, 2));
+            double y = Convert.ToDouble(getEndDate.Substring(0, 2));
+
+            numberOfDays = y - x;
+
+            var listDays = new List<WeatherforecastDto>();
+
+            for (int i = 0; i <= numberOfDays; i++)
+            {
+                listDays.Add(new WeatherforecastDto
+                {
+                    Id = i + 1,
+                    City = "Stavanger",
+                    Date = DateTime.Now.Date, //($"0{x + i}" + from.ToString().Substring(2)),
+                    Temperature = 14.3F,
+                    Windspeed = 2.5F,
+                    WindDirection = 293.8F,
+                    WindspeedGust = 5.6F,
+                    Pressure = 1023.7F,
+                    Humidity = 42.4F,
+                    ProbOfRain = 0F,
+                    AmountRain = 0F,
+                    CloudAreaFraction = 13.5F,
+                    FogAreaFraction = 0F,
+                    ProbOfThunder = 0F
+                });
+            }
+
+            return listDays;
+        }
+
+        return null;
+
+ 
+
+        //else
+        //{
+            //if(getStartDate.Substring(2, 4) != getEndDate.Substring(2, 4))
+            //{
+            //    double xmonths = Convert.ToDouble(getStartDate.Substring(2, 4));
+            //    double ymonths = Convert.ToDouble(getEndDate.Substring(2, 4));
+
+            //    if(xmonths % 2 == 0 || ymonths % 2 == 0)
+            //    {
+                    
+            //    }
+            //}
+        //}
     }
 
 }
--- BasicWebAPI/Controllers/WeatherforecastDto.cs
+++ BasicWebAPI/Controllers/WeatherforecastDto.cs
@@ -1,4 +1,22 @@
-﻿public class WeatherforecastDto
+﻿using System;
+
+public class WeatherforecastDto
 {
-    public string DateTime{ get; set; }
+
+    public int Id { get; set; }
+    public string City { get; set; }
+    public DateTime Date{ get; set; }
+    public float Temperature { get; set; }
+    public float Windspeed { get; set; }
+    public float WindDirection { get; set; }
+    public float WindspeedGust { get; set; }
+    public float Pressure { get; set; }
+
+    public float Humidity { get; set; }
+    public float ProbOfRain { get; set; }
+    public float AmountRain { get; set; }
+    public float CloudAreaFraction { get; set; }
+    public float FogAreaFraction { get; set; }
+    public float ProbOfThunder { get; set; }
+
 }
