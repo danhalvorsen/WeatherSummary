380618c13fe7d49dcc4b725b36915f52ad448c6b
Dan Edvard Halvorsen <dan.halvorsen@bouvet.no>
2022-05-24 14:51:40 +0200

    new stuff

--- WebApp/src/components/jsx-components/WeatherData.tsx
+++ WebApp/src/components/jsx-components/WeatherData.tsx
@@ -1,56 +1,43 @@
 import { render } from "@testing-library/react";
-import React from "react";
+import React, { useCallback } from "react";
 import { useState, useEffect } from "react";
 import ShowHtml from "./ShowHtml";
-import { IresultJson } from "../../Interfaces";
+import { IresultJson as IResultJson } from "../../Interfaces";
 
 type Props = {
   city: string;
   date: string;
-  fn: () => Promise<IresultJson>;
+  fn: () => Promise<Array<IResultJson>>;
 };
 
 const WeatherData = ({ city, date, fn }: Props) => {
-  const [result, setResult] = useState<Array<IresultJson>>();
-  const [mynumber, setMynumber] = useState<number>();
+  const [result, setResult] = useState<Array<IResultJson>>([]);
+
   const [htmlElement, sethtmlElement] = useState<JSX.Element[]>();
 
+  const setComponentState = useCallback(async () => {
+    const data = await fn();
+    if (data !== undefined && data !== null && data.length > 0) {
+      setResult(data);
+    }
 
-  async function setComponentState(): Promise<void | Array<IresultJson>> {
-    const getData = await fn()
-    setMynumber(19);
-    setResult(getData);
-
-   
-    //console.log(data.city);
-
-    //const city = result[0].city;
-   // const weatherStatus = result[0].weatherType;
-
-    // const listItem = result.map((item:IresultJson)=>
-    // <li key={item.toString()}>{item} </li>
-    //   );
-    // sethtmlElement(listItem)
-
-      }
+  }, [setResult, fn]);
 
 
-      // if(result !== undefined)
-      //   if(result.city !== undefined)
-      //       console.log('hello city: ' + result[0].data );
-      // console.log(result?.city);
+  const weatherData = result.map((item) =>
+  <div>{item.city}</div>);
 
-  
   return (
     <div>
       <div>
         {/* <ShowHtml data={list_items}/> */}
-        
+
       </div>
       <div id="1000">Hello!</div>
-      <button onClick={setComponentState}>CLICK ON ME</button>
-      <ul>{htmlElement}</ul>
-      <p>the city you've searched: <strong>{}</strong> and today's weather status is: <strong>{}</strong></p>
+      <button onClick={() => setComponentState()}>CLICK ON ME</button>
+      {/* <ul>{htmlElement}</ul> */}
+      <p>the city you've searched: <strong>{ }</strong> and today's weather status is: <strong>{ }</strong></p>
+      <div>{weatherData}</div>
       
     </div>
   );
--- WebApp/src/components/jsx-components/httpRequests.tsx
+++ WebApp/src/components/jsx-components/httpRequests.tsx
@@ -4,34 +4,11 @@ import { resourceLimits } from "worker_threads";
 // import { ResultFormat } from "./WeatherData";
 import { IresultJson } from "../../Interfaces";
 
-export async function MakeHttpRequest(): Promise<IresultJson> {
-  let resultvalue: IresultJson | any;
-
-  // try {
-  //   const result = axios.get<IresultJson>(`http://localhost:3000/data`); 
-  //   resultvalue = result 
-  // } catch (error) {
-  //   console.log(error);
-  // }
+export async function MakeHttpRequest(): Promise<IresultJson[]> {
   
-  axios
-    .get(`http://localhost:3000/data`)
-    .then((result) => {
-      console.log(result.data);
-      resultvalue = result.data;
-    })
-    .catch((err) => console.log(err));
 
-  
-  return resultvalue;
-
-  // let result = new Array<number>();
-  // axios.get<Array<number>>(`http://localhost:3000/source`)
-  // .then(res => result = res.data)
-  // .catch();
-  // console.log(JSON.stringify(result, null, 4));
-  // console.log(result)
-  // return result;
+  const result = await axios.get(`http://localhost:3000/data`);
+  return result.data;
 }
 
 export function MakeFakeHttpRequest(): Array<number> {
