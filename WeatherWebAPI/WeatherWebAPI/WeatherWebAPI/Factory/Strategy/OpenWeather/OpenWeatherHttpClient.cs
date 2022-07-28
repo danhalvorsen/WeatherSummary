9cbd78a3300e243b98dc1c0f1b74a81a8aacbffc
tobbasdf <103097411+tobbasdf@users.noreply.github.com>
2022-07-28 15:55:29 +0200

    dev certificate should work.

--- WeatherWebAPI/WeatherWebAPI/WeatherWebAPI/Factory/Strategy/OpenWeather/OpenWeatherHttpClient.cs
+++ WeatherWebAPI/WeatherWebAPI/WeatherWebAPI/Factory/Strategy/OpenWeather/OpenWeatherHttpClient.cs
@@ -5,14 +5,12 @@ namespace WeatherWebAPI.Factory.Strategy.OpenWeather
     public class OpenWeatherHttpClient
     {
         private readonly HttpClient _httpClient;
-        private readonly OpenWeatherConfig _openWeatherConfig;
 
-        public OpenWeatherHttpClient(OpenWeatherConfig openWeatherConfig, HttpClient httpClient)
+        public OpenWeatherHttpClient(HttpClient httpClient)
         {
             _httpClient = httpClient;
-            _openWeatherConfig = openWeatherConfig;
 
-            _httpClient.BaseAddress = _openWeatherConfig.BaseUrl;
+            _httpClient.BaseAddress = new Uri("https://api.openweathermap.org/");
 
             httpClient.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
             httpClient.DefaultRequestHeaders.Add(HeaderNames.UserAgent, "Mozilla / 5.0(Windows 10, Win64; x64; rv: 100.0) Gecko / 20100101 FireFox / 100.0");
