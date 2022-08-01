ae9d9b6212fd9544209ba5df522e4647a7c3b3d1
Mohammad <mohammad.ghasempour@bouvet.no>
2022-08-01 15:03:45 +0200

    Connect to sql database and use our services. connect to our application on Docker image.

--- WebApp/src/components/jsx-components/GetDataFromUser/SingleRequest.tsx
+++ WebApp/src/components/jsx-components/GetDataFromUser/SingleRequest.tsx
@@ -16,9 +16,10 @@ export const  SingleRequest: FC<Iprops> =  ({date , cityName})=>{
     const [result , setResult] = useState<IresultJson[] | undefined>();
     const [component , setComponent] = useState<JSX.Element >();
 
-//|
     //const source = "http://localhost:3000/data"
-    const source = "http://localhost:3000/data/?weatherType=sunny"
+    //const source = "http://localhost:3000/data/?weatherType=sunny"
+    //const source = `https://localhost:5000/api/weatherforecast/date?DateQuery.Date=${date.toISOString()}&CityQuery.City=${cityName}`;
+    const source = "https://localhost:5000/api/weatherforecast/between?BetweenDateQuery.From=2022-05-01&BetweenDateQuery.To=2022-06-21&CityQuery.City=stavanger";
     useEffect(()=>{
 
         const getRequest =( async ()=>{
