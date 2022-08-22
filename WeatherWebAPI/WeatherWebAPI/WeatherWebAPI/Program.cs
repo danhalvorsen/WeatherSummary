0641104844dc6991de629fae4129a0213173d0e4
tobbasdf <103097411+tobbasdf@users.noreply.github.com>
2022-08-22 11:20:32 +0200

    Add httpclientfactory -> TypeClient - Add TypeClient for Yr - Add TypeClient for OpenWeather - Add TypeClient for WeatherApi

--- WeatherWebAPI/WeatherWebAPI/WeatherWebAPI/Program.cs
+++ WeatherWebAPI/WeatherWebAPI/WeatherWebAPI/Program.cs
@@ -1,10 +1,10 @@
 using FluentValidation.AspNetCore;
-using Microsoft.Net.Http.Headers;
 using System.Reflection;
 using WeatherWebAPI;
 using WeatherWebAPI.Contracts;
 using WeatherWebAPI.Factory;
 using WeatherWebAPI.Factory.Strategy.OpenWeather;
+using WeatherWebAPI.Factory.Strategy.WeatherApi;
 using WeatherWebAPI.Factory.Strategy.YR;
 using WeatherWebAPI.Query;
 
@@ -22,27 +22,9 @@ builder.Services.AddAutoMapper(new List<Assembly> { Assembly.GetExecutingAssembl
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddHttpClient<YrHttpClient>();
-builder.Services.AddHttpClient<OpenWeatherHttpClient>();
-
-//builder.Services.AddHttpClient<OpenWeatherStrategy>(httpClient =>
-//{
-//    httpClient.BaseAddress = new Uri("https://api.openweathermap.org/");
-
-//    httpClient.DefaultRequestHeaders.Add(
-//        HeaderNames.Accept, "application/json");
-//    httpClient.DefaultRequestHeaders.Add(
-//        HeaderNames.UserAgent, "Mozilla / 5.0(Windows 10, Win64; x64; rv: 100.0) Gecko / 20100101 FireFox / 100.0");
-//});
-//builder.Services.AddHttpClient("Yr", httpClient =>
-//{
-//    httpClient.BaseAddress = new Uri("https://api.met.no/weatherapi/");
-
-//    httpClient.DefaultRequestHeaders.Add(
-//        HeaderNames.Accept, "application/json");
-//    httpClient.DefaultRequestHeaders.Add(
-//        HeaderNames.UserAgent, "Mozilla / 5.0(Windows 10, Win64; x64; rv: 100.0) Gecko / 20100101 FireFox / 100.0");
-//});
+builder.Services.AddHttpClient<YrStrategy>();
+builder.Services.AddHttpClient<OpenWeatherStrategy>();
+builder.Services.AddHttpClient<WeatherApiStrategy>();
 
 var app = builder.Build();
 
