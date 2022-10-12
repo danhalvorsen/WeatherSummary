27cec93c080859242f79c0bbd7ba2074ed34305c
Mohammad <mohammad.ghasempour@bouvet.no>
2022-10-12 09:50:06 +0200

    improve on navigation bar

--- WebApp/src/MyOrderingSystem/src/components/Form/NavigationBar.tsx
+++ WebApp/src/MyOrderingSystem/src/components/Form/NavigationBar.tsx
@@ -1,22 +1,27 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { CartButton } from '../cart/CartButton';
 import { useShoppingCart } from '../context/ShoppingCartContext';
-import AddProducts from '../Form/ProductsState/Products/addProducts'
 
 function NavigationBar() {
   const { loginRole } = useShoppingCart();
-  const showAddProductLink = ()=>{
+  const showAddProductLink = () => {
     if (loginRole == 'Admin') {
-      return <Link to="AddProducts">|| Add Products</Link>
+      return (
+        <span>
+          {' '}
+          || <Link to="AddProducts">Add Products</Link>
+        </span>
+      );
     }
-  }
+  };
 
   return (
     <>
       <div className=" border border-primary p-3">
-        <Link to="/">Home</Link> || <Link to="signIn">Sign In</Link>    {showAddProductLink()}
+        <Link to="/">Home</Link> || <Link to="signIn">Sign In</Link>{' '}
+        {showAddProductLink()}
         <div style={styles.leftButton}>
-          <CartButton/>
+          <CartButton />
         </div>
       </div>
     </>
--- WebApp/src/MyOrderingSystem/src/components/Form/ProductsState/Products/addProducts.tsx
+++ WebApp/src/MyOrderingSystem/src/components/Form/ProductsState/Products/addProducts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { ProductType } from '../../productType';
 import {
   addNewProduct,
